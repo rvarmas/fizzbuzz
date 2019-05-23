a3c80efdd889b921f2129058bb40b432c00784d7
Nitin2391 <nt547345@dal.ca>
2019-05-23 06:38:25 -0700

    replacing the numbers

--- task1.py
+++ task1.py
@@ -1,10 +1,15 @@
 #https://stackoverflow.com/questions/15343743/copying-from-one-text-file-to-another-using-python
+#https://stackoverflow.com/questions/41854418/how-to-replace-the-same-lowercase-and-uppercase-letters-with-a-number-in-python
 fin = open('book1.txt')
 fout = open('copy1.txt', 'w')
 
 count = 0
 
 page = 0
+
+ #if encrp_key == 1:
+     # new_msg = msg.replace('a ','1').replace('e','2')\ .replace('i','3').replace('o','4').replace('u','5')
+
 
 for y in range(page):
 	for i,val in enumerate(fin):
@@ -29,4 +34,7 @@ for y in range(page):
 				if count==25:
 					break
 
+		page +=1
+
+fount.close()
 
