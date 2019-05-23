da37bdad1812d493158dabe7810db8ae66a9224c
Nitin2391 <nt547345@dal.ca>
2019-05-23 05:54:39 -0700

    printing the numbers

--- Task0.py
+++ Task0.py
@@ -1,1 +1,18 @@
-print("Hello world")
+nums = [3,5]
+
+max = 100
+
+result = 0
+
+from num in nums:
+	for i in range(1,max):
+		if num*i < max:
+print("fizz")
+
+
+result = 0
+
+for i in range(0,max):
+	if i%3 ==0 or  i%5==0:
+
+print("buzz")
